3e95b77d527adb8953e1a8b2a1c8a509e157c864
denisnaenko <ngrinnbi@gmail.com>
2025-06-02 09:41:42 +0500

    Tailwind init + базовые стили

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,21 @@ import LinkForm from "./LinkForm.tsx";
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header"></header>
-        <LinkForm />
-    </div>
+      <div className="min-h-screen flex flex-col items-center justify-center px-4 bg-gray-100">
+          <header className="mb-8">
+              <img
+                  src="/mtc-icon.png"
+                  alt="Логотип"
+                  className="mx-auto mb-4 w-24 h-24 object-contain"
+              />
+              <h1 className="text-3xl font-bold text-gray-800 text-center">
+                  Как там Хабр?
+              </h1>
+          </header>
+          <main className="w-full max-w-lg">
+              <LinkForm />
+          </main>
+      </div>
   );
 }
 
--- frontend/src/LinkForm.tsx
+++ frontend/src/LinkForm.tsx
@@ -16,13 +16,22 @@ function LinkForm() {
     }
 
     return (
-        <div>
+        <div className="w-full max-w-md mx-auto">
             <form>
-                <input type="text" value={link} onChange={(e) => setLink(e.target.value)} />
-                <button type="submit" onClick={handleSubmit}>Суммаризировать</button>
+                <input type="text" value={link}
+                       className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                       placeholder="https://habr.com"
+                       onChange={(e) => setLink(e.target.value)} />
+                <button type="submit"
+                        className="cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg border-blue-600 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px] mx-auto block mt-4"
+                        onClick={handleSubmit}>Суммаризировать</button>
             </form>
             <div>
-                <p>{answer}</p>
+                {answer && (
+                    <div className="mt-6 p-4 bg-gray-50 rounded-lg">
+                        <p className="text-gray-700">{answer}</p>
+                    </div>
+                )}
             </div>
         </div>
     );
